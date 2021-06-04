3416395d60d90882a4d9352ec5a1339080f533bc
the-ceo22 <nsikak.essienceo@gmail.com>
2021-06-04 18:41:23 +0100

    footer implemented

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import { JumbotronContainer } from './containers/jumbotron';
+import { FooterContainer } from './containers/footer';
 
 export default function App() {
   return (
-    <JumbotronContainer />
+    <>
+     <JumbotronContainer />
+     <FooterContainer />
+    </>
   )
 };
